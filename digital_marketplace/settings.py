ce9eeaa2c3dbad5f420f6ff5f030b0ffae11cf8c
Darkripper214 <phakorn214@gmail.com>
2020-06-07 23:09:48 +0800

    First deploy

--- digital_marketplace/settings.py
+++ digital_marketplace/settings.py
@@ -148,7 +148,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 
+SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION ='none'
 LOGIN_REDIRECT_URL = '/'
 
